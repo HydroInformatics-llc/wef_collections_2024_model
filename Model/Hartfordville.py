8f4e89d43a927bad18f511718f09a72be54161e1
Bryant E. McDonnell <bemcdonnell@gmail.com>
2024-02-29 14:15:18 -0700

    Added init controls

--- Model/Hartfordville.py
+++ Model/Hartfordville.py
@@ -16,8 +16,20 @@ time controls.
 This scope of this code is to run the model as well as be a space where new control
 logic can be developed to operate the various controllable facilities.
 """
-from pyswmm import Simulation
+from pyswmm import Simulation, Links, Nodes
 
 with Simulation('./Hartfordville_1.inp') as sim:
+
+    # Creating Handles to Controllable Assets
+    old_wwtp_primary_inflow = Links(sim)['OLD_WWTP_PRIMARY_DIVERSION_GATE']
+    old_wwtp_primary_dewater = Links(sim)['OLD_WWTP_PRIMARY_DEWATER_PUMP']
+
+    # Pushing initial settings
+    def init_settings():
+        old_wwtp_primary_inflow.target_setting = 0
+        old_wwtp_primary_dewater.target_setting = 0
+
+    sim.add_before_start(init_settings)
+
     for step in sim:
         pass
